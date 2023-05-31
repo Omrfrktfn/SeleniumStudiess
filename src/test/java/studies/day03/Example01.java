1030fc719227107adcd1b7b54a7ceceeff8c0576
Omer <tufanfaruk71@gmail.com>
2023-06-01 00:52:38 +0200

    Initial commit

---
+++ src/test/java/studies/day03/Example01.java
@@ -0,0 +1,54 @@
+package studies.day03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Example01 {
+    public static void main(String[] args) {
+
+
+
+        System.setProperty("WebDriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //Techproeducation sayfasına gidelim
+        driver.get("http://techproeducation.com");
+
+        //Çıkan reklamı locate edip kapatalım
+
+        //Arama bölümünde qa aratalım
+        WebElement aramaMotoru = driver.findElement(By.id("elementor-search-form-9f26725"));
+        aramaMotoru.sendKeys("qa");
+
+        //Sayfa başlığının qa içerdiğini doğrulayalım
+        String actualName =driver.getTitle();
+        String expectedName = "qa";
+        if (actualName.equals(expectedName)){
+            System.out.println("Test PASSED");
+        }else{
+            System.out.println("Test FAILED : " + actualName);
+        }
+
+        //Carrer Opportunities In QA linkinin görünür ve erişilebilir olduğunu doğrulayalım
+
+        //Carrer Opportunities In QA linkine tıklayalım
+
+        //Başlığın Opportunities içerdiğini test edelim
+
+        //Tekrar anasayfaya dönelim ve url'in https://techproeducation.com/ olduğunu doğrulayalım
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/studies/day03/Example02.java
@@ -0,0 +1,49 @@
+package studies.day03;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Example02 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("WebDriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //https://id.heroku.com/login sayfasina gidin
+        driver.get("https://id.heroku.com/login");
+
+        //Bir mail adersi giriniz
+        WebElement mail = driver.findElement(By.id("email"));
+        mail.sendKeys("oft71@gmail.com");
+        Thread.sleep(1000);
+
+        //Bir password giriniz
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("Oft1234!");
+        Thread.sleep(1000);
+
+        //Login butonuna tiklayiniz
+        WebElement login = driver.findElement(By.name("commit"));
+        login.click();
+
+        //"There was a problem with your login." texti gorunur ise
+        //"kayit yapilamadi" yazdiriniz
+        //eger yazi gorunur degilse "kayit yapildi" yazdiriniz
+        String actualText = driver.getPageSource();
+        String expectedText = "There was a problem with your login";
+        if (actualText.contains(expectedText)){
+            System.out.println("Kayıt yapılamadı");
+        }else {
+            System.out.println("Kayıt yapıldı");
+        }
+        Thread.sleep(2000);
+
+        //sayfayı kapatiniz
+        driver.close();
+    }
+}
