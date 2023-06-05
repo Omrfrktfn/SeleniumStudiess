88d8dd09dee1dde1658cae4f89c682a085821232
Omer <tufanfaruk71@gmail.com>
2023-06-06 00:26:00 +0200

    06.06

---
+++ src/test/java/studies/day06JUnit/C02.java
@@ -0,0 +1,11 @@
+package studies.day06JUnit;
+
+public class C02 {
+
+    //- https://www.facebook.com adresine gidin
+    // - Cookies’i kabul edin
+    //- “Create an Account” button’una basin
+    //- “radio buttons” elementlerini locate edin
+    //- Secili degilse cinsiyet butonundan size uygun olani secin
+
+}
