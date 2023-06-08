25ed48e90309f7b71bde07e6fa398d277db80eea
Omer <tufanfaruk71@gmail.com>
2023-06-09 01:07:38 +0200

    09/06

---
+++ src/test/java/studies/day09JUnit/C01.java
@@ -0,0 +1,25 @@
+package studies.day09JUnit;
+
+public class C01 {
+    /*
+
+// ...Exercise1...
+// Create a new class under Q1 create main method
+// Set Path
+// Create chrome driver
+// Maximize the window
+// Open google home page https://www.google.com/.
+// On the same class, Navigate to amazon home page https://www.amazon.com/ Navigate back to google
+// Navigate forward to amazon
+// Refresh the page
+// Close/Quit the browser
+// And last step : print "all ok" on console
+    /*Exercise1... Q1 altında yeni bir sınıf oluştur ana yöntemi oluştur
+    Yolu Ayarla Krom sürücüsü oluştur Pencereyi büyüt Google ana sayfasını aç
+     https:www.google.com. Aynı sınıfta, Amazon ana sayfasına gidin
+     https:www.amazon.com Google'a geri gidin Amazon'a ilerleyin Sayfayı yenileyin
+     Kapat Tarayıcıdan çıkın Ve son adım: konsolda "tamam" yazdırın
+
+     */
+
+}
