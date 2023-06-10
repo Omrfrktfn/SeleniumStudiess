0791ca1539cfe4e5179bed76801fb59fe378c7c3
Omer <tufanfaruk71@gmail.com>
2023-06-10 14:47:40 +0200

    10.06

--- src/test/java/studies/day09JUnit/C01.java
+++ src/test/java/studies/day09JUnit/C01.java
@@ -1,5 +1,14 @@
 package studies.day09JUnit;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C01 {
     /*
 
@@ -22,4 +31,36 @@ public class C01 {
 
      */
 
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.close();
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("https:www.google.com");
+        Thread.sleep(2000);
+
+        driver.get("https:www.amazon.com");
+        Thread.sleep(2000);
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+
+    }
 }
