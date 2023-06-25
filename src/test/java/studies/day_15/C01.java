a06f7ca367bd3ba13e328e6107d5599cddd650c4
Omer <tufanfaruk71@gmail.com>
2023-06-25 15:40:48 +0200

    25/06

--- src/test/java/studies/day_15/C01.java
+++ src/test/java/studies/day_15/C01.java
@@ -1,6 +1,9 @@
 package studies.day_15;
 
 import Utilities.TestBase;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -16,17 +19,35 @@ import java.util.Date;
 
 public class C01 extends TestBase {
 
+    ExtentReports extentReports; // --> Raporlamayi baslatmak icin kullanilan class
+    ExtentHtmlReporter extentHtmlReporter;// -->Raporu HTML formatinda duzenler
+    ExtentTest extentTest;// --> Test adimlarina eklemek istedigimiz bilgileri bu class ile olustururuz.
+
     @Test
     public void test01() throws IOException {
+
+        extentReports = new ExtentReports();
+        String dosyaYolu1 = "testOutput/extentReports/extentReport.html";
+        extentHtmlReporter = new ExtentHtmlReporter(dosyaYolu1);
+        extentReports.attachReporter(extentHtmlReporter); // html formatinda raporlamayi baslaticak.
+
+        extentReports.setSystemInfo("Browser", "Chrome");
+        extentReports.setSystemInfo("Tester", "Omer");
+        extentHtmlReporter.config().setDocumentTitle("Extent Report");
+        extentHtmlReporter.config().setReportName("Smoke Test Raporu");
+
+        extentTest = extentReports.createTest("ExtentTest", "Test Raporu");
 
         //-https://jqueryui.com/slider/#colorpicker adrese gidiniz
         driver.get("https://jqueryui.com/slider/#colorpicker");
         wait(1);
+        extentTest.info("Istenilen sayfaya gidildi....");
 
         //-Kutucuk içindeki rengi önce kırmızı sonra sarı yapınız ekran goruntusunu aliniz.
 
         WebElement iframe = driver.findElement(By.xpath("//iframe"));
         driver.switchTo().frame(iframe);
+        extentTest.info("iframe islemi yapildi....");
 
         WebElement d1 = driver.findElement(By.xpath("(//*[@class='ui-slider-handle ui-corner-all ui-state-default'])[1]"));
         Actions action = new Actions(driver);
@@ -44,20 +65,30 @@ public class C01 extends TestBase {
                 moveByOffset(-160, 0).release().perform();
         wait(2);
 
+        extentTest.info("kirmizi kutu islemi yapildi");
+
         String date = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
 
         TakesScreenshot ts = (TakesScreenshot) driver;
         FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE), new File("Screenshot/screenshot" + date + ".png"));
+
+        extentTest.info("kirmizi kutu ekran goruntusu alindi");
 
 
         //        -Sarı olduğunu test edelim
         WebElement yellowSliderBar1 = driver.findElement(By.xpath("//*[@id='green']"));
         action.clickAndHold(yellowSliderBar1).moveByOffset(250, 0).release().perform();
 
+        extentTest.info("sari kutu islmei yapildi...");
+
         //src/test/java/studies/ScreenShot1
        // String dosyaYolu= "src/test/java/studies/ScreenShot1";
         String dosyaYolu = "src/test/java/studies/ScreenShot1/WEscreenShot" + date + ".jpeg";
         TakesScreenshot ts1 = (TakesScreenshot) driver;
         FileUtils.copyFile(ts1.getScreenshotAs(OutputType.FILE), new File(dosyaYolu));
+
+        extentTest.info("sari kutu ekran goruntusu alindi...");
+
+        extentReports.flush(); //bu methodu kullanmazsak raporumuz olusmaz
     }
 }
