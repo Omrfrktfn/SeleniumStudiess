97af2ab75bb5a21d1df1ef56e896bb5da3b653d0
Omer <tufanfaruk71@gmail.com>
2023-06-30 15:33:10 +0200

    30.06

--- src/test/java/studies/day_15/C04.java
+++ src/test/java/studies/day_15/C04.java
@@ -1,6 +1,7 @@
 package studies.day_15;
 
 import Utilities.TestBase;
+import com.beust.ah.A;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -8,6 +9,7 @@ import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import java.util.List;
 import java.util.Random;
 
 public class C04 extends TestBase {
@@ -19,9 +21,18 @@ public class C04 extends TestBase {
         wait(1);
 
         //Hesap makinasından 2 basamaklı random iki tane sayı seçelim ve random 4 işlemden birini yaptıralım
-        WebElement alanElement = driver.findElement(By.xpath("//*[@class='keys']"));
-        Actions action = new Actions(driver);
-        action.moveToElement(alanElement).click().perform();
+
+        for (int i = 0; i<2; i++){
+            Random rnd= new Random();
+            int random1 = rnd.nextInt();
+            List<WebElement> sayilar = driver.findElements(By.xpath("//*[@class='btn btn-outline-primary']"));
+
+
+        }
+
+
+     //   List<WebElement> sayilar = driver.findElements(By.xpath("//*[@class='btn btn-outline-primary']"));
+
 
         //Sonucu konsola yazdırıp
 
